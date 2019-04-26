bdbfd940813d505c8f6dffc33f491d4718690949
Yago Marques <yagobsmarques@gmail.com>
2019-04-26 14:47:49 -0300

    Ajeitando bug apos mexer em mesa.h

--- Codigos-Fonte/main.cpp
+++ Codigos-Fonte/main.cpp
@@ -14,8 +14,8 @@ int main(int argc, char const *argv[])
     Mesa m;
     cout<<"Bem vindo ao Blackjack ET!\n\n"<<endl;
     cout<<"Escolha uma das funcionalidades: "<<endl;
-    cout<<"     -Sair (1);"<<endl;
-    cout<<"     -Iniciar uma mesa (0);"<<endl;
+    cout<<"     -Iniciar uma mesa (1);"<<endl;
+    cout<<"     -Sair (0);"<<endl;
     cin>>n_estouro;
     while(n_estouro!=1 && n_estouro!=2){
         cout<<"\nEntre com um valor valido...n\n"<<endl;
--- Codigos-Fonte/mesa.cpp
+++ Codigos-Fonte/mesa.cpp
@@ -3,15 +3,24 @@
     }
     Mesa::~Mesa(){
     }
+    void Mesa::adicionaJogador(){
+    }
+    void Mesa::removeJogador(){
+    }
+    std::__cxx11::string Mesa::iniciaPartida(std::vector<Jogador> jogadores){
+        return "Yago";
+    }
     int Mesa::run(){
         this->aux = 1;
         while(this->aux!=0){
             cout<<"Escolha uma das funcionalidades: "<<endl;
-            cout<<"     -sair (0);"<<endl;
             cout<<"     -Adicionar jogador (1);"<<endl;
             cout<<"     -Remover um jogador jogador (2);"<<endl;
             cout<<"     -Iniciar uma partida (3);"<<endl;
+            cout<<"     -sair (0);\n"<<endl;
+            cout<<"Digite: ";
             cin>> this->aux;
+            cout<<"\n"<<endl;            
             switch (aux){
                 case 0:
                     break;
@@ -20,8 +29,10 @@
                     break;
                 case 2:
                     Mesa::removeJogador();
+                    break;
                 case 3:
                     Mesa::iniciaPartida(this->jogadores);
+                    break;
                 default:
                     cout<<"Caractere invalido :("<<endl;
                     break;
