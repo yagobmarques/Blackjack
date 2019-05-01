54a17ec79401ddc7ea8973373ee2ba5dd57507fc
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-05-01 12:31:08 -0300

    doxygen

--- Codigos-Fonte/dado.h
+++ Codigos-Fonte/dado.h
@@ -1,10 +1,16 @@
+/**
+* @file dado.h
+* @brief Cabeçalhos das funçoes
+* @author Yago Beserra Marques
+* @date 01/05/2019
+*/
 #ifndef DADO_H
 #define DADO_H
 #include <random>
 class Dado
 {
 private:
-  int valor;
+  int valor; /**< Valor aleatorio obtido*/
   std::random_device rd;
   std::default_random_engine gen;
   std::uniform_int_distribution<> dis;
