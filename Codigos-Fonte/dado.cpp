7bbf8b66b04d8c16df330bcb1833c91f1e5a2bbb
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-05-01 11:54:08 -0300

    Doxygen

--- Codigos-Fonte/dado.cpp
+++ Codigos-Fonte/dado.cpp
@@ -1,15 +1,34 @@
+/**
+* @file dado.cpp
+* @brief Arquivo .cpp que implementa as funções de dado.h
+* @author Yago Beserra Marques
+* @since 25/04/2019
+* @date 01/05/2019
+*/
 #include <random>
 #include "dado.h"
 
+/**
+* @brief Metodo construtor
+* @return Nulo
+*/
 Dado::Dado() : rd(), gen(rd()), dis(1, 6)
 {
     valor = std::round(dis(gen));
 }
+/**
+* @brief Funcao que obtem o valor aleatório de um dado
+* @return Valor randomico de 1 - 6
+*/
 int Dado::jogar()
 {
     valor = std::round(dis(gen));
     return valor;
 }
+/**
+* @brief Função que pega o valor armazenado
+* @return O prórprio valor
+*/
 int Dado::getValor()
 {
     return this->valor;
