0acaadcb5e326df012c4c9ab575deb1d3e6a87cc
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-05-01 13:06:36 -0300

    Doxygen

--- Codigos-Fonte/main.cpp
+++ Codigos-Fonte/main.cpp
@@ -1,5 +1,16 @@
+/**
+* @file main.cpp
+* @brief Método main
+* @author Yago Beserra Marques
+* @since 25/04/2019
+* @date 01/05/2019
+*/
 #include "lobby.h"
-
+/**
+* @brief Main
+* @param argc Quantidade de parametros e argv O valor dos parâmetros
+* @return inteiro
+*/
 int main(int argc, char const *argv[])
 {
     Lobby jogo;
