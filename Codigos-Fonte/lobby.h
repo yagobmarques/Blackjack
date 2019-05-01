46344072b99e731d13dfccb3e46e1c3d832d28bb
Yago Marques <44405378+yagobmarques@users.noreply.github.com>
2019-05-01 13:04:16 -0300

    Doxygen

--- Codigos-Fonte/lobby.h
+++ Codigos-Fonte/lobby.h
@@ -1,3 +1,10 @@
+/**
+* @file lobby.h
+* @brief Arquivo .ch que declara as funções para lobby.h
+* @author Yago Beserra Marques
+* @since 25/04/2019
+* @date 01/05/2019
+*/
 #include <iostream>
 #include <string>
 #include "jogador.h"
@@ -13,7 +20,7 @@ class Lobby
 private:
     /* data */
 public:
-    Lobby(/* args */);
-    ~Lobby();
-    int runGame();
+    Lobby(/* args */); /**< Costrutor*/
+    ~Lobby(); /**< Destrutor */
+    int runGame(); /**< Inicia um jogo ou sai do programa */
 };
